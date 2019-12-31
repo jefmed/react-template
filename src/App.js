42713000bc713fd08124d5853164647d954e32d4
Jeferson Medeiros <jeferson.medeiros@gmail.com>
2019-12-31 13:33:24 -0300

    set eslint, prettier e editorConfig

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Hello Mama!</h1>
+  return (
+    <div className="App">
+      <h1>Hello Mama!</h1>
+    </div>
+  );
 }
 
 export default App;
